aa2d556340c93cf5293af719f833cff3b80d136d
Nico Baier <nico.baier@gmail.com>
2023-07-18 13:25:54 -0500

    added a fetch val code command

---
+++ src/fetchCode.tsx
@@ -0,0 +1,47 @@
+import { Detail, Toast, getPreferenceValues, showToast } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { useEffect, useState } from "react";
+import createActionPanel from "./ActionPanel";
+import { ValInfo } from "./types";
+import { buildExpressEndpoint, buildRunEndpoint, buildValtownURL, codeblock } from "./utils";
+
+const { apiToken } = getPreferenceValues();
+
+export default function Command(props: { arguments: Arguments.FetchCode }) {
+  const valInfo: ValInfo = {
+    valname: props.arguments.valname.split(".")[1],
+    username: props.arguments.valname.split(".")[0].startsWith("@")
+      ? props.arguments.valname.split(".")[0].substring(1)
+      : props.arguments.valname.split(".")[0],
+  };
+
+  const valtownURL = buildValtownURL(valInfo);
+  const runEndpoint = buildRunEndpoint(valInfo);
+  const expressEndpoint = buildExpressEndpoint(valInfo);
+
+  const [markdown, setMarkdown] = useState<string>();
+
+  const { isLoading, data, revalidate } = useFetch(
+    `https://api.val.town/v1/alias/${valInfo.username}/${valInfo.valname}`,
+    {
+      onError: (error) => {
+        console.error(error);
+        setMarkdown("");
+        showToast(Toast.Style.Failure, "Sorry, that Val could not found.");
+      },
+    }
+  );
+
+  useEffect(() => {
+    //@ts-ignore
+    setMarkdown(codeblock(data.code, "ts"));
+  }, [data]);
+
+  return (
+    <Detail
+      isLoading={isLoading}
+      markdown={markdown}
+      actions={createActionPanel(valtownURL, runEndpoint, expressEndpoint)}
+    />
+  );
+}
