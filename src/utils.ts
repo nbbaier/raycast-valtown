0fd102577c3c911d3928fc5cbce54a77fe507f96
Nico Baier <nico.baier@gmail.com>
2023-07-18 01:21:51 -0500

    added ability to open a val directly

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,28 @@
+import { test } from "node:test";
 import { ValInfo } from "./types";
 
-export function buildExpressEndpoint(valInfo: ValInfo) {
-  if (valInfo.username.startsWith("@")) {
-    valInfo.username = valInfo.username.substring(1);
+const testInfo: ValInfo = {
+  username: "@nbbaier",
+  valname: "hello",
+};
+
+function formatUsername(username: string): string {
+  if (username.startsWith("@")) {
+    return username.substring(1);
   }
-  return `https://${valInfo.username}-${valInfo.valname}.express.val.run`;
+  return username;
+}
+
+export function buildValtownURL(valInfo: ValInfo): string {
+  return `https://www.val.town/v/${formatUsername(valInfo.username)}.${valInfo.valname}`;
+}
+
+export function buildExpressEndpoint(valInfo: ValInfo) {
+  return `https://${formatUsername(valInfo.username)}-${valInfo.valname}.express.val.run`;
 }
 
 export function buildRunEndpoint(valInfo: ValInfo) {
-  if (valInfo.username.startsWith("@")) {
-    valInfo.username = valInfo.username.substring(1);
-    console.log(valInfo.username);
-  }
-  return `https://api.val.town/v1/run/${valInfo.username}.${valInfo.valname}`;
+  return `https://api.val.town/v1/run/${formatUsername(valInfo.username)}.${valInfo.valname}`;
 }
 
 export function codeblock(code: string, language: string) {
